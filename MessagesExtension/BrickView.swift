b68b575fc6bc7c7cb3e316331291437c15865955
Warren Whipple <modalrealist@gmail.com>
2016-08-07 14:59:52 -0400

    Update to Swift 3 beta 4

---
+++ MessagesExtension/BrickView.swift
@@ -0,0 +1,21 @@
+//
+//  BrickView.swift
+//  VoteBubble
+//
+//  Created by Warren Whipple on 8/7/16.
+//  Copyright © 2016 Warren Whipple. All rights reserved.
+//
+
+import UIKit
+
+class BrickView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MessagesExtension/UserSettings.swift
@@ -0,0 +1,9 @@
+//
+//  UserSettings.swift
+//  VoteBubble
+//
+//  Created by Warren Whipple on 8/2/16.
+//  Copyright © 2016 Warren Whipple. All rights reserved.
+//
+
+import Foundation
