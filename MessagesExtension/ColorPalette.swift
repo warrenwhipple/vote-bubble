ffef2d33eb6c881850867e2f6a768cef93a8ee98
Warren Whipple <modalrealist@gmail.com>
2018-07-26 21:48:07 -0400

    Pull out of drawer and dust off

--- MessagesExtension/ColorPalette.swift
+++ MessagesExtension/ColorPalette.swift
@@ -13,5 +13,4 @@ enum ColorPalette {
     static let messageBackground   = #colorLiteral(red: 0.8831103444, green: 0.8879093528, blue: 0.9005897641, alpha: 1) // Slightly darker than screenshot
     static let uiBlue              = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1) // From screenshot
 
-
 }
